08aafe8ff3671c204293ea4dc6bb4b74d1f51ed5
rfblock <javascriptcubing999@gmail.com>
2022-04-09 10:50:51 -0400

    Added movie and ratings data structures

---
+++ movie.py
@@ -0,0 +1,8 @@
+class Movie:
+    def __init__(self, id, title, description, length, youtube_trailer, ratings):
+        self.id = id
+        self.title = title
+        self.description = description
+        self.length = length
+        self.youtube_trailer = youtube_trailer
+        self.ratings = ratings
---
+++ ratings.py
@@ -0,0 +1,5 @@
+class Ratings:
+    def __init__(self, imdb, metacritic, rotten_tomatoes)
+    self.imdb = imdb
+    self.metacritic = metacritic
+    self.rotten_tomatoes = rotten_tomatoes
